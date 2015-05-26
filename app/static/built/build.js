9534cc936102a945de9821570a5b45b2927f13d5
Jeff Roche <jkeauctions@gmail.com>
2015-05-26 11:32:09 -0400

    Built version of app with minified css and js.

---
+++ app/static/built/build.js
@@ -0,0 +1,1 @@
+({mainConfigFile:"js/main.js",appDir:".",baseUrl:"js",removeCombined:!0,findNestedDependencies:!0,dir:"built",optimizeCss:"standard",modules:[{name:"main",exclude:["jquery","underscore","backbone","bootstrap"]}],paths:{jquery:"empty:",underscore:"empty:",backbone:"empty:",bootstrap:"empty:"}});
